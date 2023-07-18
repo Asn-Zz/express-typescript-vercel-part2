77db778e4a7cb770da50a56f3f7e3938d7227c91
Asn-Zz <44092815+Asn-Zz@users.noreply.github.com>
2023-07-18 09:21:20 +0800

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors({
+  origin: '*', // 允许的源
+  methods: ['GET', 'POST'], // 允许的HTTP方法
+  allowedHeaders: ['Content-Type', 'Authorization'] // 允许的自定义请求头
+}));
 
 app.get('/', (_req: Request, res: Response) => {
 	return res.send('Express Typescript on Vercel')
