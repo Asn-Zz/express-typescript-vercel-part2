e3e3e295fbd82db31f764bc3f162436df9559fb8
Asn-Zz <44092815+Asn-Zz@users.noreply.github.com>
2023-07-18 08:53:22 +0800

    feat: init

--- dist/index.js
+++ dist/index.js
@@ -5,12 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
 });
-app.get('/ping', (_req, res) => {
-    return res.send('pong 🏓');
+let logs = [];
+app.get('/log', (_req, res) => {
+    if (_req.query.m) {
+        logs.push(_req.query.m);
+    }
+    const body = JSON.stringify(logs, null, 4);
+    return res.send(body);
+});
+app.get('/log/reset', (_req, res) => {
+    logs = [];
+    return res.send('ok');
 });
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from 'express'
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.get('/', (_req: Request, res: Response) => {
 	return res.send('Express Typescript on Vercel')
 })
 
-app.get('/ping', (_req: Request, res: Response) => {
-	return res.send('pong 🏓')
+let logs: any[] = []
+app.get('/log', (_req: Request, res: Response) => {
+	if (_req.query.m) {
+		logs.push(_req.query.m)
+	}
+	const body = JSON.stringify(logs, null, 4)
+
+	return res.send(body)
+})
+
+app.get('/log/reset', (_req: Request, res: Response) => {
+	logs = []
+	
+	return res.send('ok')
 })
 
 app.listen(port, () => {
